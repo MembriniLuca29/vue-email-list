25ca9ff87c0dbc6dd4c2ec207b48c8eeff2ee93f
MembriniLuca29 <lucamembrini29@gmail.com>
2023-07-05 14:58:47 +0200

    bonus completato

--- js/script.js
+++ js/script.js
@@ -3,15 +3,23 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      emails: [] 
+      emails: [], 
+      isLoad: false 
     }
   },
   created() {
+    let sonoDieci = 0; 
+
     for (let i = 0; i < 10; i++) {
       axios
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
-          this.emails.push(response.data.response); 
+          this.emails.push(response.data.response);
+
+          sonoDieci++;
+          if (sonoDieci === 10) {
+            this.isLoad = true; 
+          }
         });
     }
   }
